775b816056b1c1e1ae749a528bd298193000ece5
ahmedgomaaeid <pahmedgomaaeid@gmail.com>
2023-10-06 11:09:27 +0300

    website

---
+++ Website/js/popup.js
@@ -0,0 +1,39 @@
+AOS.init();
+$(document).ready(function(){
+    $("#show1").click(function(){
+        $("#pop1").css("display","block");
+    });
+    $("#close1").click(function(){
+        $("#pop1").css("display","none");
+    });
+    $("#show2").click(function(){
+        $("#pop2").css("display","block");
+    });
+    $("#close2").click(function(){
+        $("#pop2").css("display","none");
+    });
+    $("#show3").click(function(){
+        $("#pop3").css("display","block");
+    });
+    $("#close3").click(function(){
+        $("#pop3").css("display","none");
+    });
+    $("#show4").click(function(){
+        $("#pop4").css("display","block");
+    });
+    $("#close4").click(function(){
+        $("#pop4").css("display","none");
+    });
+    $("#show5").click(function(){
+        $("#pop5").css("display","block");
+    });
+    $("#close5").click(function(){
+        $("#pop5").css("display","none");
+    });
+    $("#show6").click(function(){
+        $("#pop6").css("display","block");
+    });
+    $("#close6").click(function(){
+        $("#pop6").css("display","none");
+    });
+});
